65cb58b1c322b9843ebebd7f2bdd3982e8e9ff05
sabudou <68882987+sabudou@users.noreply.github.com>
2020-10-26 18:55:14 -0500

    Add files via upload

---
+++ QuickDBD-export -ETL.sql
@@ -0,0 +1,37 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/bfc4D5
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "career_stats_adj2" (
+    "Name" VARCHAR   NOT NULL,
+    "GP" INT   NOT NULL,
+    "G" INT   NOT NULL,
+    "A" INT   NOT NULL,
+    "TP" INT   NOT NULL,
+    "PPG" VARCHAR   NOT NULL,
+    "PIM" VARCHAR   NOT NULL,
+    "Period" VARCHAR   NOT NULL,
+    "Years" VARCHAR   NOT NULL,
+    "POS" VARCHAR   NOT NULL,
+    "firstYear" INT   NOT NULL,
+    "lastYear" INT   NOT NULL,
+    CONSTRAINT "pk_career_stats_adj2" PRIMARY KEY (
+        "Name"
+     )
+);
+
+CREATE TABLE "master_adj" (
+    "Name" VARCHAR   NOT NULL,
+    "firstName" VARCHAR   NOT NULL,
+    "lastName" VARCHAR   NOT NULL,
+    "playerID" VARCHAR   NOT NULL,
+    "hofID" VARCHAR   NOT NULL,
+    "firstNHL" INT   NOT NULL,
+    "lastNHL" INT   NOT NULL,
+    "pos" VARCHAR   NOT NULL
+);
+
+ALTER TABLE "career_stats_adj2" ADD CONSTRAINT "fk_career_stats_adj2_Name" FOREIGN KEY("Name")
+REFERENCES "master_adj" ("Name");
+
